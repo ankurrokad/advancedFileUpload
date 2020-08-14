4f1c978f9ee0b9f74c28282b93d69bc311ebf959
Ankur.Rokad <priyanshi.jariwala@coruscate.info>
2020-08-14 13:16:01 +0530

    Update, Dynamic Path

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/file-upload', async (req, res) => {
     try {
-        let type = 2
+        let type = 1
 
         // let config = {
         //     path: './upload'
